e14412338c48e941c1d9e9612f378964998cdccc
AhmedEWVA <67864173+AhmedEWVA@users.noreply.github.com>
2023-05-31 16:39:13 +0000

    removing some lines

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import recall_score, precision_recall_fscore_support, accuracy_score, precision_score, f1_score
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.decomposition import PCA
 from sklearn import svm, tree, linear_model, neighbors, naive_bayes, ensemble, discriminant_analysis, gaussian_process
 
 # Import necessary libraries
@@ -20,7 +19,7 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
 import os
-# ...
+
 app = Flask(__name__)
 pkl_filename = os.getcwd() + "/final_model.pkl"
 with open(pkl_filename, 'rb') as file:
@@ -48,5 +47,3 @@ if __name__ == "__main__":
 
 
 #http://127.0.0.1:8001/search?GP=36&MIN=27.4&PTS=7.4&GM=2.7&FGA=7.6&FGP=34.7&three_P_Made=0.5&three_PA=2.1&three_PP=52&FTM=7&FTA=2&FTP=3&OREB=4&DREB=5&REB=6&ST=7&STL=5&BLK=2&TOV=4
-# 
-# 
